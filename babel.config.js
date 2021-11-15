02237f66e8da3e6e0cc12aa7c27acaba51e7e8df
Danilo Oliveira <danilo@sigalei.com.br>
2021-11-15 19:20:51 -0300

    chore: remove console.log

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = (api) => {
   // This caches the Babel config
   api.cache.using(() => process.env.NODE_ENV);
-  console.log('xxxx', !api.env('production'))
+
   return {
     presets: [
       '@babel/react',
