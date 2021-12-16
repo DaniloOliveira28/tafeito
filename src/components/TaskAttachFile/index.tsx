735df58aae4fd3bc39b5f84779b9ed38efbb7355
Danilo Oliveira <danilo@sigalei.com.br>
2021-12-16 19:23:09 -0300

    Revert "Revert "feat: add upload file""
    This reverts commit 79167f8b858a863f733875b088e6ba61c9c6a828.

--- src/components/TaskAttachFile/index.tsx
+++ src/components/TaskAttachFile/index.tsx
@@ -5,20 +5,59 @@ import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box'
 import { styled } from '@mui/material/styles';
- 
+import { useAxios } from '../../hooks/useAxios';
+
 
 const Input = styled('input')({
   display: 'none',
 });
 
+type TaskAttachFileProps = {
+  taskId: number;
+  updateTasks: () => void;
+}
 
-const TaskAttachFile = () => {
+const TaskAttachFile = (props: TaskAttachFileProps) => {
+  const {
+    taskId
+  } = props;
+  const {
+    commit,
+    response,
+    loading
+  } = useAxios({
+    method: 'POST',
+    path: `tarefas/${taskId}/anexos`
+  });
+
+  const fileUpload = (file:File) => {
+    const url = 'http://example.com/file-upload';
+    const formData = new FormData();
+    formData.append('arquivo',file)
+    const customHeaders = {
+      'content-type': 'multipart/form-data'
+    }
+
+    commit(formData,
+      () => {
+        props.updateTasks()
+      }, 
+      `tarefas/${taskId}/anexos`, 
+      undefined,
+      customHeaders)
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.files?.[0]) {
+      fileUpload(e.target.files?.[0])
+    }
+  }
 
   return (
-    <Tooltip title='Adicionar Anexo'>
+    <Tooltip title={`Adicionar Anexo ${taskId}`}>
       <Box>
-      <label htmlFor="icon-button-file">
-        <Input accept={"*/*"} id="icon-button-file" type="file" />
+      <label htmlFor={`icon-button-file_${taskId}`}>
+        <Input id={`icon-button-file_${taskId}`} accept={"*/*"} type="file" onChange={onChange}/>
         <IconButton edge="end" aria-label="anexos" component="span">
           <AttachFile />
         </IconButton>
--- src/components/TasksListWrapper/index.tsx
+++ src/components/TasksListWrapper/index.tsx
@@ -29,7 +29,7 @@ const TasksListWrapper = (props:TasksListWrapperProps) => {
         const catTasks = tasks.filter(task => task.id_categoria === category.id);
         return [category, catTasks]
       })
-      return finalTasks.map(finalTask => (<TasksList updateTasks={updateTasks} key={`category_${finalTask[0].id}`} category={finalTask[0]} tasks={finalTask[1]} />))
+      return finalTasks.map(finalTask => (<Box key={`category_${finalTask[0].id}`} ><TasksList updateTasks={updateTasks} category={finalTask[0]} tasks={finalTask[1]} /></Box>))
     }
   }
   return (
